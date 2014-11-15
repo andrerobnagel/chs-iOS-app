616d2b0fdd774e1a62a0874a10b074a410bedd18
andrerobnagel <andrerobnagel@gmail.com>
2014-11-15 15:53:39 -0700

    Initial Commit

---
+++ RebelNet/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RebelNet
+//
+//  Created by Andre on 11/15/14.
+//  Copyright (c) 2014 Andre. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RebelNet/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RebelNet
+//
+//  Created by Andre on 11/15/14.
+//  Copyright (c) 2014 Andre. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
